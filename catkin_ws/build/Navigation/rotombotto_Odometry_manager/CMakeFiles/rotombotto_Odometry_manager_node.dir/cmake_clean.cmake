6b7aaea95113bc2eb2531c33f6906addf422c535
dang13 <daniel.garcesmarin@gmail.com>
2019-11-06 22:12:23 -0600

    Creacion de paquetes NavigationStack

---
+++ catkin_ws/build/Navigation/rotombotto_Odometry_manager/CMakeFiles/rotombotto_Odometry_manager_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rotombotto_Odometry_manager_node.dir/src/rotombotto_Odometry_manager_node.cpp.o"
+  "/home/danielg/RotomBotto/catkin_ws/devel/lib/rotombotto_Odometry_manager/rotombotto_Odometry_manager_node.pdb"
+  "/home/danielg/RotomBotto/catkin_ws/devel/lib/rotombotto_Odometry_manager/rotombotto_Odometry_manager_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rotombotto_Odometry_manager_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
